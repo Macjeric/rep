a7b1f0890b1465aa2b26a693799adf6e6e1ab16c
Macjeric <mack.jeric8@gmail.com>
2020-10-24 00:29:55 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Reneal</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+        
+
+        <!-- Styles -->
+        <link  href="/css/main.css" rel="stylesheet"  >
+    
+        <style>
+            body {
+                font-family: 'Open Sans', sans-serif;
+            }
+        </style>
+    </head>
+
+    <div class="contain">
+        <div class="flex justify-center mt-4">   
+            <img src="/imgs/ren.png" width="100" height="100">
+        </div>
+
+        <div class="flex justify-center">
+            <h2 class="mt-1">
+                Reaneal International Education Outreach Inc.
+            </h2> 
+        </div>
+
+        <div class="flex justify-center">
+            <h3>
+                 Longido Installation
+            </h3>
+        </div>
+
+        <p class="ml-94">
+            Thurs, 22 Oct 2020
+        </p>
+
+        <h4>
+            Checklist for Lab Installation
+        </h4>
+        
+        <p>
+            School : Engarenaibor
+        </p>
+        <div>
+            <table class="table-auto">
+  <thead>
+    <tr>
+      <th class="px-4 py-2">Title</th>
+      <th class="px-4 py-2">Author</th>
+      <th class="px-4 py-2">Views</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border px-4 py-2">Install Network Switch</td>
+      <td class="border px-4 py-2">Adam</td>
+      <td class="border px-4 py-2">858</td>
+    </tr>
+    <tr class="bg-gray-100">
+      <td class="border px-4 py-2">Set up server, UPS, voltage stabilizer</td>
+      <td class="border px-4 py-2">Adam</td>
+      <td class="border px-4 py-2">112</td>
+    </tr>
+    <tr>
+      <td class="border px-4 py-2">Plug in Ethernet cables to clients [one-by-one test, add labels]</td>
+      <td class="border px-4 py-2">Chris</td>
+      <td class="border px-4 py-2">1,280</td>
+    </tr>
+    <tr class="bg-gray-100">
+      <td class="border px-4 py-2">Reset BIOS of clients</td>
+      <td class="border px-4 py-2">Adam</td>
+      <td class="border px-4 py-2">112</td>
+    </tr>
+    <tr>
+      <td class="border px-4 py-2">Number computers</td>
+      <td class="border px-4 py-2">Chris</td>
+      <td class="border px-4 py-2">1,280</td>
+    </tr>
+    <tr>
+      <td class="border px-4 py-2">Memory check</td>
+      <td class="border px-4 py-2">Chris</td>
+      <td class="border px-4 py-2">1,280</td>
+    </tr>
+    <tr>
+      <td class="border px-4 py-2">Remind computer teacher to create teacher and student spreadsheets</td>
+      <td class="border px-4 py-2">Chris</td>
+      <td class="border px-4 py-2">1,280</td>
+    </tr>
+    <tr>
+      <td class="border px-4 py-2">Provide donation letter</td>
+      <td class="border px-4 py-2">Chris</td>
+      <td class="border px-4 py-2">1,280</td>
+    </tr>
+    <tr>
+      <td class="border px-4 py-2">Install Network Switch</td>
+      <td class="border px-4 py-2">Adam</td>
+      <td class="border px-4 py-2">858</td>
+    </tr>
+    <tr class="bg-gray-100">
+      <td class="border px-4 py-2">Set up server, UPS, voltage stabilizer</td>
+      <td class="border px-4 py-2">Adam</td>
+      <td class="border px-4 py-2">112</td>
+    </tr>
+    <tr>
+      <td class="border px-4 py-2">Plug in Ethernet cables to clients [one-by-one test, add labels]</td>
+      <td class="border px-4 py-2">Chris</td>
+      <td class="border px-4 py-2">1,280</td>
+    </tr>
+    <tr class="bg-gray-100">
+      <td class="border px-4 py-2">Reset BIOS of clients</td>
+      <td class="border px-4 py-2">Adam</td>
+      <td class="border px-4 py-2">112</td>
+    </tr>
+  </tbody>
+</table>
+
+        </div>       
+    </div>
+
+  
+        
+    
+</html>
