7840b8f2dde80cc7a1b2f891a7f51ab23896977c
Macjeric <mack.jeric8@gmail.com>
2020-10-24 13:19:59 +0300

    font+imgs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,14 @@
         <title>Reneal</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-
+        <!-- <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"> -->
+           <style type="text/css">
+                @font-face {
+                    font-family: "Adobe Clean UI";
+                    src: url(/fonts/AdobeCleanUI-Regular.ttf) format("truetype");
+                }
+                
+</style>
         
 
         <!-- Styles -->
@@ -16,7 +22,7 @@
     
         <style>
             body {
-                font-family: 'Open Sans', sans-serif;
+                font-family: Adobe Clean UI;
             }
         </style>
     </head>
@@ -38,7 +44,7 @@
             </h3>
         </div>
 
-        <p class="ml-94">
+        <p class="ml-84">
             Thurs, 22 Oct 2020
         </p>
 
@@ -46,83 +52,109 @@
             Checklist for Lab Installation
         </h4>
         
-        <p>
-            School : Engarenaibor
-        </p>
-        <div>
-            <table class="table-auto">
-  <thead>
-    <tr>
-      <th class="px-4 py-2">Title</th>
-      <th class="px-4 py-2">Author</th>
-      <th class="px-4 py-2">Views</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td class="border px-4 py-2">Install Network Switch</td>
-      <td class="border px-4 py-2">Adam</td>
-      <td class="border px-4 py-2">858</td>
-    </tr>
-    <tr class="bg-gray-100">
-      <td class="border px-4 py-2">Set up server, UPS, voltage stabilizer</td>
-      <td class="border px-4 py-2">Adam</td>
-      <td class="border px-4 py-2">112</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2">Plug in Ethernet cables to clients [one-by-one test, add labels]</td>
-      <td class="border px-4 py-2">Chris</td>
-      <td class="border px-4 py-2">1,280</td>
-    </tr>
-    <tr class="bg-gray-100">
-      <td class="border px-4 py-2">Reset BIOS of clients</td>
-      <td class="border px-4 py-2">Adam</td>
-      <td class="border px-4 py-2">112</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2">Number computers</td>
-      <td class="border px-4 py-2">Chris</td>
-      <td class="border px-4 py-2">1,280</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2">Memory check</td>
-      <td class="border px-4 py-2">Chris</td>
-      <td class="border px-4 py-2">1,280</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2">Remind computer teacher to create teacher and student spreadsheets</td>
-      <td class="border px-4 py-2">Chris</td>
-      <td class="border px-4 py-2">1,280</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2">Provide donation letter</td>
-      <td class="border px-4 py-2">Chris</td>
-      <td class="border px-4 py-2">1,280</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2">Install Network Switch</td>
-      <td class="border px-4 py-2">Adam</td>
-      <td class="border px-4 py-2">858</td>
-    </tr>
-    <tr class="bg-gray-100">
-      <td class="border px-4 py-2">Set up server, UPS, voltage stabilizer</td>
-      <td class="border px-4 py-2">Adam</td>
-      <td class="border px-4 py-2">112</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2">Plug in Ethernet cables to clients [one-by-one test, add labels]</td>
-      <td class="border px-4 py-2">Chris</td>
-      <td class="border px-4 py-2">1,280</td>
-    </tr>
-    <tr class="bg-gray-100">
-      <td class="border px-4 py-2">Reset BIOS of clients</td>
-      <td class="border px-4 py-2">Adam</td>
-      <td class="border px-4 py-2">112</td>
-    </tr>
-  </tbody>
-</table>
+        <div class="flex justify-between">
+            
+            <div><p>
+                School : Engarenaibor
+            </p></div>
 
-        </div>       
+           <div class="flex"><img class="mr-2" src="/imgs/pin.png" width="25" height="20">
+            -2.534056,36.492222
+           </div>
+                
+                
+        </div>
+
+        <div>
+                <table class="table-auto mt-5">
+                  <thead>
+                    <tr>
+                      <th class="px-4 py-2">List</th>
+                            
+                      <th class="px-4 py-2">Check</th>
+                      <th class="px-4 py-2">Time</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="border px-4 py-2">Install Network Switch</td>
+                      <td class="border px-4 py-2"><img src="/imgs/check.png" width="30" height="30"></td>
+                      <td class="border px-4 py-2">10:50</td>
+                    </tr>
+                    <tr class="bg-gray-100">
+                      <td class="border px-4 py-2">Set up server, UPS, voltage stabilizer</td>
+                      <td class="border px-4 py-2"><img src="/imgs/check.png" width="30" height="30"></td>
+                      <!-- <td class="border px-4 py-2">112</td> -->
+                    </tr>
+                    <tr>
+                      <td class="border px-4 py-2">Plug in Ethernet cables to clients [one-by-one test, add labels]</td>
+                      <td class="border px-4 py-2"><img src="/imgs/check.png" width="30" height="30"></td>
+                      <!-- <td class="border px-4 py-2">1,280</td> -->
+                    </tr>
+                    <tr class="bg-gray-100">
+                      <td class="border px-4 py-2">Reset BIOS of clients</td>
+                      <td class="border px-4 py-2"><img src="/imgs/check.png" width="30" height="30"></td>
+                      <!-- <td class="border px-4 py-2">112</td> -->
+                    </tr>
+                    <tr>
+                      <td class="border px-4 py-2">Number computers</td>
+                      <td class="border px-4 py-2"><img src="/imgs/check.png" width="30" height="30"></td>
+                      <!-- <td class="border px-4 py-2">1,280</td> -->
+                    </tr>
+                    <tr>
+                      <td class="border px-4 py-2">Memory check</td>
+                      <td class="border px-4 py-2"><img src="/imgs/check.png" width="30" height="30"></td>
+                      <!-- <td class="border px-4 py-2">1,280</td> -->
+                    </tr>
+                    <tr>
+                      <td class="border px-4 py-2">Remind computer teacher to create teacher and student spreadsheets</td>
+                      <td class="border px-4 py-2"><img src="/imgs/check.png" width="30" height="30"></td>
+                      <!-- <td class="border px-4 py-2">1,280</td> -->
+                    </tr>
+                    <tr>
+                      <td class="border px-4 py-2">Provide donation letter</td>
+                      <td class="border px-4 py-2"><img src="/imgs/check.png" width="30" height="30"></td>
+                      <td class="border px-4 py-2">14:44</td>
+                    </tr>
+                    
+                  </tbody>
+                </table>
+
+        </div>   
+
+
+        <div class="mt-5">
+            <p class="text-xl font-semibold">Issues</p>
+            <p>
+                There were no issues found.
+            </p>
+        </div>
+
+        <hr></hr>
+        <div >
+            <p class="mt-8 font-medium">
+            Participants:</p><p>
+            Evance Kessy, Elibariki Melami, Anorld Christopher, Collins Moshi, Ashura Yusuph.
+        </p>    
+        <p class="mt-1">
+            Robert Nyangaka, Erick Macky.
+        </p>
+        </div><hr>
+
+        <div class=" flex justify-center mt-8">
+            <img src="/imgs/anm.png" width="80" height="80">
+
+            <img src="/imgs/ren.png" width="80" height="80">
+
+        </div> 
+
+        <div>
+            <p>
+                habari.co.tz | reneal.org
+            </p>
+        </div>
+
+
     </div>
 
   
